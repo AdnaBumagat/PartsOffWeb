61abe71957e31c62fd839fe9ba0c7dfff2429cb5
AdnaBumagat <adna.bumagat.up@phinmaed.com>
2024-02-19 01:28:35 +0800

    "create and list category"

--- resources/views/admin/category/create.blade.php
+++ resources/views/admin/category/create.blade.php
@@ -9,7 +9,7 @@
                 <h1>Create Category</h1>
             </div>
             <div class="col-sm-6 text-right">
-                <a href="categories.html" class="btn btn-primary">Back</a>
+                <a href="{{route('categories.index')}}" class="btn btn-primary">Back</a>
             </div>
         </div>
     </div>
@@ -27,12 +27,14 @@
                             <div class="mb-3">
                                 <label for="name">Name</label>
                                 <input type="text" name="name" id="name" class="form-control" placeholder="Name">
+                                <p></p>
                             </div>
                         </div>
                         <div class="col-md-6">
                             <div class="mb-3">
                                 <label for="slug">Slug</label>
-                                <input type="text" name="slug" id="slug" class="form-control" placeholder="Slug">
+                                <input type="text"  name="slug" id="slug" class="form-control" placeholder="Slug">
+                                <p></p>
                             </div>
                         </div>
                         <div class="col-md-6">
@@ -49,7 +51,7 @@
             </div>
             <div class="pb-5 pt-3">
                 <button type="submit" class="btn btn-primary">Create</button>
-                <a href="#" class="btn btn-outline-dark ml-3">Cancel</a>
+                <a href="{{route('categories.index')}}" class="btn btn-outline-dark ml-3">Cancel</a>
             </div>
         </form>
     </div>
@@ -63,12 +65,54 @@
 $("#categoryForm").submit(function(event){
     event.preventDefault();
     var element = $(this);
+    $("button[type=submit]").prop('disabled',true);
+
     $.ajax({
         url:'{{route("categories.store")}}',
         type:'post',
         data: element.serializeArray(),
         dataType:'json',
         success: function(response){
+            $("button[type=submit]").prop('disabled',false);
+
+            if(response["status"]== true){
+
+                window.location.href="{{route('categories.index')}}";
+
+                $("#name").removeClass('is-invalid')
+                    .siblings('p')
+                    .removeClass('invalid-feedback').html([""]);
+
+                $("#slug").removeClass('is-invalid')
+                    .siblings('p')
+                    .removeClass('invalid-feedback').html([""]);
+
+            }else{
+                var errors = response['errors'];
+                if(errors['name']){
+                    $("#name").addClass('is-invalid')
+                    .siblings('p')
+                    .addClass('invalid-feedback').html(errors['name']);
+
+                }else{
+                    $("#name").removeClass('is-invalid')
+                    .siblings('p')
+                    .removeClass('invalid-feedback').html([""]);
+
+                }
+
+                if(errors['slug']){
+                    $("#slug").addClass('is-invalid')
+                    .siblings('p')
+                    .addClass('invalid-feedback').html(errors['name']);
+
+                }else{
+                    $("#slug").removeClass('is-invalid')
+                    .siblings('p')
+                    .removeClass('invalid-feedback').html([""]);
+
+                }
+            }
 
         }, error: function(jqXHR, exception){
             console.log("something went wrong");
@@ -77,5 +121,21 @@ $("#categoryForm").submit(function(event){
     })
 
 });
+    $("#name").change(function(){
+        element = $(this);
+        $("button[type=submit]").prop('disabled',true);
+        $.ajax({
+            url:'{{route("getSlug")}}',
+            type:'get',
+            data: {title: element.val()},
+            dataType:'json',
+            success: function(response){
+                $("button[type=submit]").prop('disabled',false);
+                if (response["status"] == true){
+                    $("#slug").val(response["slug"]);
+                }
+            }
+        });
+    });
 </script>
 @endsection
