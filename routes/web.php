f63000f77f81ce0b840968fe0a614dc4b8bed4ee
VernPherus <vernpherus@gmail.com>
2024-02-19 15:39:35 +0800

    created edit and delete categories

--- routes/web.php
+++ routes/web.php
@@ -46,6 +46,9 @@ Route::group(['prefix'=>'admin'],function(){
         Route::get('/categories',[CategoryController::class,'index'])->name('categories.index');
         Route::get('/categories/create',[CategoryController::class,'create'])->name('categories.create');
         Route::post('/categories',[CategoryController::class,'store'])->name('categories.store');
+        Route::get('/categories/{categories}/edit',[CategoryController::class,'edit'])->name('categories.edit');
+        Route::put('/categories/{categories}',[CategoryController::class,'update'])->name('categories.update');
+        Route::delete('/categories/{categories}',[CategoryController::class,'destroy'])->name('categories.delete');
 
         //temp-images.create
         Route::post('/upload-temp-image',[TempImagesController::class,'create'])->name('temp-images.create');
