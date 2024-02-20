8c87eee9093bc126114ba050b82810e69c2faa22
Ra1zuu <raizuryuya@gmail.com>
2024-02-20 18:43:16 +0800

    added views, frontend, authorization

--- routes/web.php
+++ routes/web.php
@@ -5,8 +5,9 @@ use App\Http\Controllers\admin\CategoryController;
 use App\Http\Controllers\admin\HomeController;
 use App\Http\Controllers\admin\ProductController;
 use App\Http\Controllers\admin\TempImagesController;
-use Illuminate\Support\Facades\Request;
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\FrontController;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Str;
 
@@ -27,46 +28,66 @@ use Illuminate\Support\Str;
 //     return view('welcome');
 // });
 
+//main page
 Route::get('/', [FrontController::class, 'index'])->name('front.home');
 
+//admin login
 Route::get('/admin/login',[AdminLoginController::class,'index'])->name('admin.login');
 
+//Login and Register Routes, localhost/account/login or localhost/account/register
+Route::group(['prefix'=>'account'],function(){
+    Route::group(['middleware'=>'guest'],function(){
+        Route::get('/login',[AuthController::class,'login'])->name('account.login');
+        Route::get('/post',[AuthController::class,'authenticate'])->name('account.authenticate');
+
+        Route::get('/register',[AuthController::class,'register'])->name('account.register');
+        Route::post('/process-register',[AuthController::class,'processRegister'])->name('account.processRegister');
+    });
+
+//logout and profile section
+    Route::group(['middleware'=>'auth'],function(){
+        Route::get('/profile',[AuthController::class,'profile'])->name('account.profile');
+        Route::get('/logout',[AuthController::class,'logout'])->name('account.logout');
+    });
+});
+
+//admin middleware
 Route::group(['prefix'=>'admin'],function(){
-    Route::group(['middleware'=>'admin.guest'],function(){
+Route::group(['middleware'=>'admin.guest'],function(){
 
-        Route::get('/login',[AdminLoginController::class,'index'])->name('admin.login');
-        Route::post('/authenticate',[AdminLoginController::class,'authenticate'])->name('admin.authenticate');
-    });
+Route::get('/login',[AdminLoginController::class,'index'])->name('admin.login');
+Route::post('/authenticate',[AdminLoginController::class,'authenticate'])->name('admin.authenticate');
+});
 
-    Route::group(['middleware'=>'admin.auth'],function(){
+Route::group(['middleware'=>'admin.auth'],function(){
+Route::get('/dashboard',[HomeController::class,'index'])->name('admin.dashboard');
+Route::get('/logout',[HomeController::class,'logout'])->name('admin.logout');
 
-        Route::get('/dashboard',[HomeController::class,'index'])->name('admin.dashboard');
-        Route::get('/logout',[HomeController::class,'logout'])->name('admin.logout');
+//Category Routes
+Route::get('/categories',[CategoryController::class,'index'])->name('categories.index');
 
-        //Category Routes
-        Route::get('/categories',[CategoryController::class,'index'])->name('categories.index');
+Route::get('/categories/create',[CategoryController::class,'create'])->name('categories.create');
+Route::post('/categories',[CategoryController::class,'store'])->name('categories.store');
 
-        Route::get('/categories/create',[CategoryController::class,'create'])->name('categories.create');
-        Route::post('/categories',[CategoryController::class,'store'])->name('categories.store');
 
-        //temp-images.create
-        Route::post('/upload-temp-image',[TempImagesController::class,'create'])->name('temp-images.create');
+//temp-images.create
+Route::post('/upload-temp-image',[TempImagesController::class,'create'])->name('temp-images.create');
 
-        Route::get('/getSlug',function(Request $request){
-            $slug ='';
-            if(!empty($request->title)){
-                $slug = Str::slug($request->title);
-            }
-            return response()->json([
-                'status'=>true,
-                'slug' => $slug
+Route::get('/getSlug',function(Request $request){
+$slug ='';
+if(!empty($request->title)){
+$slug = Str::slug($request->title);
+}
+return response()->json([
+'status'=>true,
+'slug' => $slug
 
-            ]);
-        })->name('getSlug');
+]);
+})->name('getSlug');
 
-        //Product Routes
-        Route::get('/products/create',[ProductController::class,'create'])->name('products.create');
-        Route::post('/products',[ProductController::class,'store'])->name('products.store');
-    });
+//Product Routes
+ Route::get('/products/create',[ProductController::class,'create'])->name('products.create');
+Route::post('/products',[ProductController::class,'store'])->name('products.store');
+});
 
 });
