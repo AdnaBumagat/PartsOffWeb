0a0c6bf9655a1c590563797ee3c5434f4d8a3851
VernPherus <vernpherus@gmail.com>
2024-03-03 13:16:30 +0800

    Added seeders, and edited gitignores

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -12,5 +12,72 @@ class CategorySeeder extends Seeder
      */
     public function run(): void
     {
+
+        $categories = array(
+            array(
+                'name' => 'GPU',
+                'slug' => 'GPU',
+                'image' => null,
+                'status' => '1',
+                'showHome' => 'Yes'
+            ),
+            array(
+                'name' => 'Processor',
+                'slug' => 'Processor',
+                'image' => null,
+                'status' => '1',
+                'showHome' => 'Yes'
+            ),
+            array(
+                'name' => 'RAM Stick',
+                'slug' => 'RAM Stick',
+                'image' => null,
+                'status' => '1',
+                'showHome' => 'Yes'
+            ),
+            array(
+                'name' => 'PC Case',
+                'slug' => 'PC Case',
+                'image' => null,
+                'status' => '1',
+                'showHome' => 'Yes'
+            ),
+            array(
+                'name' => 'Storage',
+                'slug' => 'Storage',
+                'image' => null,
+                'status' => '1',
+                'showHome' => 'Yes'
+            ),
+            array(
+                'name' => 'Monitors',
+                'slug' => 'Monitors',
+                'image' => null,
+                'status' => '1',
+                'showHome' => 'Yes'
+            ),
+            array(
+                'name' => 'Keyboards',
+                'slug' => 'Keyboards',
+                'image' => null,
+                'status' => '1',
+                'showHome' => 'Yes'
+            ),
+            array(
+                'name' => 'Mouse',
+                'slug' => 'Mouse',
+                'image' => null,
+                'status' => '1',
+                'showHome' => 'Yes'
+            ),
+            array(
+                'name' => 'Cables',
+                'slug' => 'Cables',
+                'image' => null,
+                'status' => '1',
+                'showHome' => 'Yes'
+            )
+        );
+
     }
 }
