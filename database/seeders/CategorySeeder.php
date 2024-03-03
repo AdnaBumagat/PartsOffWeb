952dfe3016675d5722f12396f0a237e2b88dc654
VernPherus <vernpherus@gmail.com>
2024-03-03 13:30:43 +0800

    fixed seeders

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -2,8 +2,9 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class CategorySeeder extends Seeder
 {
@@ -78,6 +79,8 @@ class CategorySeeder extends Seeder
                 'showHome' => 'Yes'
             )
         );
+
+        DB::table('categories')->insert($categories);
 
     }
 }
