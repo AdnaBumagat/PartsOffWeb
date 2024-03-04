e1b857aea04cd47ec1b879cbd7377753250bedd2
AdnaBumagat <adna.bumagat.up@phinmaed.com>
2024-03-04 21:57:21 +0800

    " add favicon"

--- resources/views/front/layouts/app.blade.php
+++ resources/views/front/layouts/app.blade.php
@@ -4,6 +4,7 @@
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
     <title>Parts OFF</title>
+    <link rel="icon" href="{{asset('favicon.png')}}">
     <meta name="description" content="" />
     <meta name="viewport"
         content="width=device-width, initial-scale=1, shrink-to-fit=no, maximum-scale=1, user-scalable=no" />
